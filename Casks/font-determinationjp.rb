cc49ee79f127acc9f800cd9650f657a7c12e87ed
5ouma <101255979+5ouma@users.noreply.github.com>
2022-12-05 19:27:09 +0900

    [clean] Change to install fonts directly
    Change to install from official site.

--- Casks/font-determinationjp.rb
+++ Casks/font-determinationjp.rb
@@ -2,7 +2,7 @@ cask "font-determinationjp" do
   version "1"
   sha256 "d97a69a6e559cdf13cfc1db6a838a7ae65bf4df1d32548acb3ed8db60c0c6c13"
 
-  url "https://gitfront.io/r/Souma/ga3p8eqLP2dE/Fonts/raw/fonts/DeterminationJP/DeterminationJP.ttf"
+  url "https://thup.work/miniup/?mode=dl&id=7330&original=1"
   name "DeterminationJP"
   desc "Grand Chaos Productions. Based on spritesheet by Leafia_Barrett. Modified by theFIZZYnator. Undertale is copyright and intellectual property of Toby Fox."
   homepage "https://twitter.com/tomathinn_UT/status/716251705835323393"
---
+++ Casks/font-giro.rb
@@ -0,0 +1,12 @@
+cask "font-giro" do
+  version "001.000"
+  sha256 "de94652566eaf88f188072f1fb2100f3d2c13789fb554bf21c233c29b041b3d7"
+
+  url "https://dl.dafont.com/dl/?f=giro"
+  name "Giro Light"
+  desc "Giro is an informal sans serif typeface."
+  homepage "http://www.marcelomagalhaes.net"
+
+  font "girolight001.otf"
+  font "girooutline001.otf"
+end
