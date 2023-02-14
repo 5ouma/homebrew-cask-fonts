2cd3b4efc59cad60cdb627f64f2fd49ef42b41c3
5ouma <101255979+5ouma@users.noreply.github.com>
2023-02-14 15:52:45 +0900

    [upgrade] Version up Splatoon fonts
    Upgrade round and runic fonts.

--- Casks/font-splatoon-round.rb
+++ Casks/font-splatoon-round.rb
@@ -1,11 +1,11 @@
 cask "font-splatoon-round" do
   version "1.0"
-  sha256 "cfef26584721cc5c85aea34993506d4e0a8c8daad50d47547f294193c547dab4"
+  sha256 "015737f62cdc968f08fce28300f4c88ef8646f7e7595a0d6a8adb81ca575d07c"
 
-  url "https://drive.google.com/u/0/uc?id=1HayFd8CUeK09jZ1GR6F1rEIm_ng5OHyF"
+  url "https://drive.google.com/u/0/uc?id=10gQuOlWeOHTtz5MaAPCMwSrUC8XBbRBM"
   name "inkling v3 Regular"
   desc "Script deciphered by and font created by jacebeleren.tumblr.com."
   homepage "https://twitter.com/ardnin_/status/1535737230949490695"
 
-  font "SplatoonRound.TTF"
+  font "SplatoonRoundV2.otf"
 end
