1b95fa1ccf02ee7d159aa1cb507f2e1e18bac495
5ouma <101255979+5ouma@users.noreply.github.com>
2023-12-01 21:26:06 +0900

    fix(line-seed-jp): Update file path
    To fit the original one, add version for the folder name.

--- Casks/font-line-seed-jp.rb
+++ Casks/font-line-seed-jp.rb
@@ -1,5 +1,5 @@
 cask "font-line-seed-jp" do
-  version "1.009"
+  version "20230831"
   sha256 :no_check
 
   url "https://seed.line.me/src/images/fonts/LINE_Seed_JP.zip"
@@ -7,8 +7,8 @@ cask "font-line-seed-jp" do
   desc "Seed, a word containing the meaning of growth, is LINE's first custom typeface"
   homepage "https://seed.line.me/"
 
-  font "LINE_Seed_JP/Desktop/OTF/LINESeedJP_OTF_Th.otf"
-  font "LINE_Seed_JP/Desktop/OTF/LINESeedJP_OTF_Rg.otf"
-  font "LINE_Seed_JP/Desktop/OTF/LINESeedJP_OTF_Bd.otf"
-  font "LINE_Seed_JP/Desktop/OTF/LINESeedJP_OTF_Eb.otf"
+  font "LINESeedJP_#{version}/Desktop/OTF/LINESeedJP_OTF_Th.otf"
+  font "LINESeedJP_#{version}/Desktop/OTF/LINESeedJP_OTF_Rg.otf"
+  font "LINESeedJP_#{version}/Desktop/OTF/LINESeedJP_OTF_Bd.otf"
+  font "LINESeedJP_#{version}/Desktop/OTF/LINESeedJP_OTF_Eb.otf"
 end
