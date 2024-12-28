8c474546eb5a67e69e7ba08ca62560d3e740010b
Souma <101255979+5ouma@users.noreply.github.com>
2024-12-28 09:57:10 +0900

    fix(cask): Correct the version strategy
    The character "v" should not be placed in the version section.

--- Casks/font-nerd-fonts.rb
+++ Casks/font-nerd-fonts.rb
@@ -1,8 +1,8 @@
 cask "font-nerd-fonts" do
-  version "v3.3.0"
+  version "3.3.0"
   sha256 "2078603c1e7a2fc2fa9e625ba1c30264d5d7c39907813d89beaa373f73a3a340"
 
-  url "https://github.com/ryanoasis/nerd-fonts/releases/download/#{version}/NerdFontsSymbolsOnly.zip",
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/NerdFontsSymbolsOnly.zip",
       verified: "github.com/"
   name "Nerd Fonts"
   desc "Iconic font aggregator, collection, and patcher"
