de5619d20c15c5ed64959ca5c72532aef16116ab
Souma <101255979+5ouma@users.noreply.github.com>
2024-12-28 16:52:21 +0900

    chore(cask): Change LINE Seed JP to GitHub Releases
    It's possible to bump up automatically.

--- Casks/font-line-seed-jp.rb
+++ Casks/font-line-seed-jp.rb
@@ -1,14 +1,15 @@
 cask "font-line-seed-jp" do
-  version "20241105"
-  sha256 :no_check
+  version "20241007"
+  sha256 "1e885e9bff2b68647576ff50cdd7f0d69f3776ba3af0f10052dd122aca57e5ff"
 
-  url "https://seed.line.me/src/images/fonts/LINE_Seed_JP.zip"
+  url "https://github.com/line/seed/releases/download/v#{version}/LINESeed-fonts.zip",
+      verified: "github.com/"
   name "LINE Seed JP"
   desc "Seed, a word containing the meaning of growth, is LINE's first custom typeface"
   homepage "https://seed.line.me/"
 
-  font "LINESeedJP_#{version}/Desktop/OTF/LINESeedJP_OTF_Th.otf"
-  font "LINESeedJP_#{version}/Desktop/OTF/LINESeedJP_OTF_Rg.otf"
-  font "LINESeedJP_#{version}/Desktop/OTF/LINESeedJP_OTF_Bd.otf"
-  font "LINESeedJP_#{version}/Desktop/OTF/LINESeedJP_OTF_Eb.otf"
+  font "LINESeed-fonts/LINESeedJP/fonts/otf/LINESeedJP-Bold.otf"
+  font "LINESeed-fonts/LINESeedJP/fonts/otf/LINESeedJP-ExtraBold.otf"
+  font "LINESeed-fonts/LINESeedJP/fonts/otf/LINESeedJP-Regular.otf"
+  font "LINESeed-fonts/LINESeedJP/fonts/otf/LINESeedJP-Thin.otf"
 end
