57a27b9f5a68e22a30e15e29ad4a7e0b79cb3688
Souma <101255979+5ouma@users.noreply.github.com>
2025-04-21 20:18:49 +0900

    chore(font): Remove the unnecessary options
    Make it clean and short.

--- Casks/font-natadecoco-gothic.rb
+++ Casks/font-natadecoco-gothic.rb
@@ -2,7 +2,7 @@ cask "font-natadecoco-gothic" do
   version :latest
   sha256 :no_check
 
-  url "https://kashika-labo.com/download/natadecoco-gothic-zip/?wpdmdl=1295&refresh=64f31112e32231693651218"
+  url "https://kashika-labo.com/download/natadecoco-gothic-zip/?wpdmdl=1295"
   name "Natadecoco gothic"
   desc "柔らかな幾何学図形が固まって生まれた優しい欧文フリーフォント"
   homepage "https://kashika-labo.com/natadecoco-gothic"
